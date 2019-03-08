cd7c6f6a0331f7d10fea0571fadf825d79728556
SenpaiAkaSenpai <jakub.gagat@onet.pl>
2019-03-08 08:45:24 +0100

    na wyscigach wyscigowych

--- inc/template.php
+++ inc/template.php
@@ -58,7 +58,7 @@
     </section>
 
     <!-- Footer -->
-    <footer class="py-5 bg-dark">
+    <footer class="py-5 bg-dark fixed-bottom">
       <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
       </div>
